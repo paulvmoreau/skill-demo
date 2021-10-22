1985b17f31169708bebf36e3ded2b7b0c8805760
Paul Moreau <paulvmore@gmail.com>
2021-10-22 11:48:40 -0500

    firebase config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,19 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBi0LvdNZleOvpPpZv9EXsQ9YqQ-aZRcaM",
+  authDomain: "skill-demo-41dd3.firebaseapp.com",
+  projectId: "skill-demo-41dd3",
+  storageBucket: "skill-demo-41dd3.appspot.com",
+  messagingSenderId: "542168834515",
+  appId: "1:542168834515:web:c15a2bdc27c200675ccf26",
+  measurementId: "G-VRYPK69560"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 @Component({
   selector: 'app-root',
