18d37062c85b24572b103c847c2a577480a656cf
Paul Moreau <paulvmore@gmail.com>
2022-02-10 13:41:35 -0600

    spelling

--- src/app/services/url.service.ts
+++ src/app/services/url.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class UrlService {
   private prodBase = 'https://us-central1-skill-demo-41dd3.cloudfunctions.net/';
-  private localBass = 'http://localhost:5001/skill-demo-41dd3/us-central1/';
+  private localBase = 'http://localhost:5001/skill-demo-41dd3/us-central1/';
   constructor() { }
 
   get presets(): any {
